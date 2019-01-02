81623a1be50f615a3c3b613dd2e48ecc435d5118
Brendan Eddy <brendane@uw.edu>
2019-01-02 14:44:32 -0800

    first commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+doomdisplay:
+	gcc main.c -o doomdisplay -I include -L lib -l SDL2-2.0.0
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+int main() {
+    printf("Hello World\n");
+    SDL_Init(SDL_INIT_VIDEO);
+    return 0;
+}
