5ef0fee5224b8616960c9a050371957c752e22d5
Brendan Eddy <brendane@cs.washington.edu>
2019-01-03 22:48:38 -0800

    SDL setup

--- main.c
+++ main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
 int main() {
-    printf("Hello World\n");
-    SDL_Init(SDL_INIT_VIDEO);
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0) {
+        printf("error initializing SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("DOOM style 3D visual display",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+    if (!window) {
+        printf("error creating window: %s\n", SDL_GetError());
+        SDL_Quit();
+	    return 1;
+    }
+
+    SDL_Renderer *rend = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!rend) {
+        printf("error creating renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+   
+    // do stuff with renderer, maybe pixel textures in a separate fcn?
+
+    SDL_DestroyRenderer(rend);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+
     return 0;
 }
